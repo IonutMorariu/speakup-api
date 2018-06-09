2f57827d29eaf2dca9f98c675f0fc87dd71991b2
Ionut Morariu <ionut.morariu.c@gmail.com>
2018-06-09 13:04:44 +0200

    Fixed Schema error

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema();
+const Schema = mongoose.Schema;
 const mongodbErrorHandler = require('mongoose-mongodb-errors');
 const validator = require('validator');
 mongoose.Promise = global.Promise;
