f1e59255e98e8c6ae65e5e0fecd5aba14c83e633
Ionut Morariu <ionut.morariu.c@gmail.com>
2018-06-19 13:39:38 +0200

    Check for existing chat

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -13,10 +13,24 @@ exports.startChat = async (req, res) => {
 	}
 	const matchingUsers = await User.find({ native_language: user.learning_language });
 	console.log(matchingUsers);
-	const randomNumber = Math.floor(Math.random() * matchingUsers.length);
-	const randomUser = matchingUsers[randomNumber];
+	let randomNumber = Math.floor(Math.random() * matchingUsers.length);
+	let randomUser = matchingUsers[randomNumber];
 	if (!randomUser) {
 		res.status(404).send('No matching user found');
+	}
+	let chatExists = true;
+	let count = 0;
+	while (chatExists && count < matchingUsers.length) {
+		const existingChat = await Chat.findOne({
+			$or: [{ user_1: user._id, user_2: randomUser._id }, { user_1: randomUser._id, user_2: user._id }]
+		});
+		if (!existingChat) {
+			chatExists = false;
+		} else {
+			randomNumber = Math.floor(Math.random() * matchingUsers.length);
+			randomUser = matchingUsers[randomNumber];
+			count++;
+		}
 	}
 	const newChat = await new Chat({
 		user_1: user._id,
@@ -34,7 +48,8 @@ exports.getChats = async (req, res) => {
 	const userId = req.body.user._id; //<- Comes from checkSession middleware
 	const chats = await Chat.find({ $or: [{ user_1: userId }, { user_2: userId }] })
 		.populate('user_1')
-		.populate('user_2');
+		.populate('user_2')
+		.select('-session_token');
 	if (!chats) {
 		res.status(404).send('No chats found');
 		return;
